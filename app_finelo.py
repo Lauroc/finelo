bcff4bc1b28c5d3c57ddd28e6c984815b902db04
Lauroc <8416090+Lauroc@users.noreply.github.com>
2022-05-29 16:29:27 +0300

    Update app_finelo.py
    2 containers

--- app_finelo.py
+++ app_finelo.py
@@ -3,7 +3,7 @@ import streamlit as st
 import plotly.express as px
 
 
-st.set_page_config(page_title="Finnish elo",page_icon=":soccer:", layout="wide") #page_icon=":bar_chart:"
+st.set_page_config(page_title="Finnish elo",page_icon=":soccer:") #, layout="wide") #page_icon=":bar_chart:"
 
 @st.cache
 def get_data():
@@ -14,19 +14,22 @@ def get_data():
 
 
 hist_graph=st.container()
+table = st.container()
 
 
 with hist_graph:
-    ranking, hist=get_data()
-    ranking, hist=get_data()
     st.title('Finnish football elo')
+    ranking, hist=get_data()
     teams = st.multiselect('Teams', ranking.index,
                            default=['KuPS', "HJK", 'FC Honka','Inter Turku','SJK Seinäjoki' ])
-    
-    fig = px.line(hist[teams][300:])
+    fig = px.line(hist[teams][280:])
     fig.update_layout(xaxis_rangeslider_visible=True)
-
     st.write(fig)
+    
+with table:
+    st.subheader('Ranking')
+    ranking, hist=get_data()
+
     
     st.table(ranking.style.format("{:7.0f}"))
     st.text('Last update: 28/05/2022')
